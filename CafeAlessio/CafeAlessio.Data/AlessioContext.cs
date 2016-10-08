1350f9d0c88e232056a3e5716ec6ea20915dd72d
Lilian <Lilian@LILIAN-PC>
2016-10-08 21:31:56 +0200

    New classes added

---
+++ CafeAlessio/CafeAlessio.Data/AlessioContext.cs
@@ -0,0 +1,23 @@
+﻿using CafeAlessio.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeAlessio.Data
+{
+    public class AlessioContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        //public AlessioContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserEntity>()
+            .ToTable("User");
+        }
+    }
+}
---
+++ CafeAlessio/CafeAlessio.Data/Repositories/EntityBaseRepository.cs
@@ -0,0 +1,101 @@
+﻿using CafeAlessio.Data.Abstract;
+using CafeAlessio.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeAlessio.Data.Repositories
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private AlessioContext _context;
+
+        #region Properties
+        public EntityBaseRepository(AlessioContext context)
+        {
+            _context = context;
+        }
+        #endregion
+        public virtual IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().AsEnumerable();
+        }
+
+        public virtual int Count()
+        {
+            return _context.Set<T>().Count();
+        }
+        public virtual IEnumerable<T> AllIncluding(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            foreach (var includeProperty in includeProperties)
+            {
+                query = query.Include(includeProperty);
+            }
+            return query.AsEnumerable();
+        }
+
+        public T GetSingle(int id)
+        {
+            return _context.Set<T>().FirstOrDefault(x => x.Id == id);
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().FirstOrDefault(predicate);
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            foreach (var includeProperty in includeProperties)
+            {
+                query = query.Include(includeProperty);
+            }
+
+            return query.Where(predicate).FirstOrDefault();
+        }
+
+        public virtual IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().Where(predicate);
+        }
+
+        public virtual void Add(T entity)
+        {
+            DbEntityEntry dbDbEntityEntry = _context.Entry<T>(entity);
+            _context.Set<T>().Add(entity);
+        }
+
+        public virtual void Update(T entity)
+        {
+            DbEntityEntry dbDbEntityEntry = _context.Entry<T>(entity);
+            dbDbEntityEntry.State = EntityState.Modified;
+        }
+        public virtual void Delete(T entity)
+        {
+            DbEntityEntry dbDbEntityEntry = _context.Entry<T>(entity);
+            dbDbEntityEntry.State = EntityState.Deleted;
+        }
+
+        public virtual void DeleteWhere(Expression<Func<T, bool>> predicate)
+        {
+            IEnumerable<T> entities = _context.Set<T>().Where(predicate);
+
+            foreach (var entity in entities)
+            {
+                _context.Entry<T>(entity).State = EntityState.Deleted;
+            }
+        }
+
+        public virtual void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
